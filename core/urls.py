2984b2cd59f0b4f8182365a35ddf7bd08866bc78
Str2hex <srthex@mail.ru>
2022-11-27 00:22:58 +0300

    remove slash to core.urls path('signup'

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from core.views import CreateUserApiView
 
 urlpatterns = [
-    path('signup/', CreateUserApiView.as_view(), name='signup'),
+    path('signup', CreateUserApiView.as_view(), name='signup'),
 
 ]
