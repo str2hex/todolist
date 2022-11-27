daf9f3701aed210acc2b52cc8237359dbb6f68ac
Str2hex <srthex@mail.ru>
2022-11-27 14:59:25 +0300

    add update user profile #2

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('signup', views.CreateUserApiView.as_view(), name='signup'),
     path('login', views.LoginUserView.as_view(), name='login'),
-    path('profile', views.LoginUserView.as_view(), name='profile'),
+    path('profile', views.ProfileUserView.as_view(), name='profile'),
 
 ]
