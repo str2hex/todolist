46e2024fa690e79a406fa4bd7f3ad7653bfa281e
Str2hex <srthex@mail.ru>
2022-11-27 14:57:01 +0300

    add update user profile

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from core.views import CreateUserApiView, LoginUserView
+from . import views
 
 urlpatterns = [
-    path('signup', CreateUserApiView.as_view(), name='signup'),
-    path('login', LoginUserView.as_view(), name='login'),
+    path('signup', views.CreateUserApiView.as_view(), name='signup'),
+    path('login', views.LoginUserView.as_view(), name='login'),
+    path('profile', views.LoginUserView.as_view(), name='profile'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,19 +1,21 @@
-from django.contrib.auth import get_user_model, login
-from rest_framework.generics import CreateAPIView, GenericAPIView
+from django.contrib.auth import get_user_model, login, logout
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, GenericAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-from core.serializers import CreateUserSerializers, LoginUserSerializers
+from . import serializers
 
 USER_MODEL = get_user_model()
 
 
 class CreateUserApiView(CreateAPIView):
     queryset = USER_MODEL
-    serializer_class = CreateUserSerializers
+    serializer_class = serializers.CreateUserSerializers
 
 
 class LoginUserView(GenericAPIView):
-    serializer_class = LoginUserSerializers
+    serializer_class = serializers.LoginUserSerializers
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
@@ -21,3 +23,17 @@ class LoginUserView(GenericAPIView):
         user = serializer.save()
         login(request=request, user=user)
         return Response(serializer.data)
+
+
+class ProfileUserView(RetrieveUpdateDestroyAPIView):
+    queryset = USER_MODEL.object.all()
+    serializer_class = serializers.ProfilesSerializers
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+    def delete(self, request, *args, **kwargs):
+        logout(request)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
