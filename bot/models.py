9643cac8064ec62739b355fc2d1a931cca2e241d
Str2hex <srthex@mail.ru>
2022-12-15 11:00:24 +0300

    Добавил модель юзера телеграмма

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from core.models import User
+
 
 # Create your models here.
+class TgUser(models.Model):
+    chat_id = models.IntegerField()
+    user_ud = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
